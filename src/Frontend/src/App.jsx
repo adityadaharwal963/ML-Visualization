5f20e6cc15a541a6d77fa4f5698d692272b8b09e
Riya Jaiswal <riyajaiswal@Shilpas-MacBook-Air.local>
2025-02-04 16:55:22 +0530

    updating frontend

--- src/Frontend/src/App.jsx
+++ src/Frontend/src/App.jsx
@@ -3,7 +3,6 @@ import Menu from './template/menu.jsx';
 import FileUpload from './template/fileUpload.jsx';
 import GraphToCode from './template/GraphToCode.jsx';
 
-<<<<<<< HEAD
 function App() {
   const [activeComponent, setActiveComponent] = useState('FileUpload');
 
@@ -17,34 +16,6 @@ function App() {
       {activeComponent === 'FileUpload' && <FileUpload />}
       {activeComponent === 'GraphToCode' && <GraphToCode />}
     </div>
-=======
-import '@xyflow/react/dist/style.css';
-
-import GraphToCodeTemplate from './Template/GraphToCode.template.jsx';
-import BaseTemplate from './Template/Base.template';
-import Message from './components/prompt/message';
-import Flowchart from './components/flowchart/flowchart'
-
-export default function App() {
-
-  return (
-    <>
-      <GraphToCodeTemplate />
-
-
-      <div className="flex flex-col items-end justify-center w-screen min-h-screen bg-red-300 text-gray-800">
-
-        {/* Prompt section */}
-        <Message />
-
-
-        {/* Flowchart section */}
-        <Flowchart />
-
-      </div>
-
-    </>
->>>>>>> origin
   );
 
 }
